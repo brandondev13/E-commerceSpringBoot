c81bdcef489285976a79e35734f75d5c31c70bd8
Brandon Mejia <brandonmejiarestrepo@gmail.com>
2022-12-09 08:41:19 -0500

    Mostrar los datos del producto en otra vista

--- ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
+++ ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.ecommerce.ajsanta.controller;
 
 import com.ecommerce.ajsanta.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductoService productoService;
@@ -19,6 +24,17 @@ public class HomeController {
 
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+
+    @GetMapping("/productohome/{id}")
+    public String productoHome(@PathVariable Integer id) {
+
+        LOGGER.info("Id producto enviado como parametro {}", id);
+
+
+
+
+        return "usuario/productohome";
     }
 
 
