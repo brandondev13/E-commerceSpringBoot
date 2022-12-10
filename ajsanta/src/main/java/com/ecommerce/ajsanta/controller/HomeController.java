af579bc293cd5aa11e906eb908ec7fd986355e69
Brandon Mejia <brandonmejiarestrepo@gmail.com>
2022-12-10 17:31:21 -0500

    Mostrar el carrito desde la opcion carrito del menú

--- ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
+++ ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
@@ -69,9 +69,12 @@ public class HomeController {
         detalleOrden.setProducto(producto);
 
 
-        detalles.add(detalleOrden);
-
-
+        // Validar que producto no se añada 2 veces
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+        if(!ingresado) {
+            detalles.add(detalleOrden);
+        }
 
         sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
@@ -109,6 +112,14 @@ public class HomeController {
         return "usuario/carrito";
     }
 
+    @GetMapping("/getCart")
+    public String getCart(Model model) {
+
+
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+        return "usuario/carrito";
+    }
 
 
 
