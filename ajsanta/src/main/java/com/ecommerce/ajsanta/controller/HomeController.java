ee4485012f64c7e91bdb6547d4c77bad2aca0e06
Brandon Mejia <brandonmejiarestrepo@gmail.com>
2022-12-10 13:27:51 -0500

    Redireccionar a la vista carrito

--- ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
+++ ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -38,10 +39,12 @@ public class HomeController {
         producto  = productoOptional.get();
 
         model.addAttribute("producto", producto);
-
-
-
         return "usuario/productohome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart() {
+        return "usuario/carrito";
     }
 
 
