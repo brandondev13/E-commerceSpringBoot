0617bd96726e2a14a8983674bb91779363584ae6
Brandon Mejia <brandonmejiarestrepo@gmail.com>
2022-12-10 16:49:37 -0500

    Enviar los productos y la orden a la vista, mostrar los productos en la vista carrito

--- ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
+++ ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
@@ -51,7 +51,7 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
 
         DetalleOrden detalleOrden = new DetalleOrden();
         Producto producto = new Producto();
@@ -60,6 +60,25 @@ public class HomeController {
         Optional<Producto> optionalProducto = productoService.get(id);
         LOGGER.info("Producto añadido: {}", optionalProducto.get());
         LOGGER.info("Cantidad: {}", cantidad);
+        producto = optionalProducto.get();
+
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setPrecio(producto.getPrecio());
+        detalleOrden.setNombre(producto.getNombre());
+        detalleOrden.setTotal(producto.getPrecio() * cantidad);
+        detalleOrden.setProducto(producto);
+
+
+        detalles.add(detalleOrden);
+
+
+
+        sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
+        orden.setTotal(sumaTotal);
+
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
 
         return "usuario/carrito";
     }
