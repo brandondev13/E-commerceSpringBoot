7cd36b26a02dc3d8b01961b6707ccbbc79e200f5
Brandon Mejia <brandonmejiarestrepo@gmail.com>
2022-12-11 13:15:26 -0500

    Implementamos la funcionalidad registro de usuario

--- ajsanta/src/main/java/com/ecommerce/ajsanta/controller/UsuarioController.java
+++ ajsanta/src/main/java/com/ecommerce/ajsanta/controller/UsuarioController.java
@@ -1,14 +1,21 @@
 package com.ecommerce.ajsanta.controller;
 
+import com.ecommerce.ajsanta.model.Usuario;
 import com.ecommerce.ajsanta.service.IUsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UsuarioController.class);
+
 
     @Autowired
     private IUsuarioService usuarioService;
@@ -16,8 +23,17 @@ public class UsuarioController {
 
     // /usuario/registro
     @GetMapping("/registro")
-    public String registro() {
+    public String create() {
         return "usuario/registro";
+    }
+
+    @PostMapping("/save")
+    public String save(Usuario usuario) {
+        LOGGER.info("Usuario registro: {}", usuario);
+        usuario.setTipo("USER");
+        usuarioService.save(usuario);
+        
+        return "redirect:/";
     }
 
 
