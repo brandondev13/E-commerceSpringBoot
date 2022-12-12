dae92908036741c18386c7a23190b184296a2722
Brandon Mejia <brandonmejiarestrepo@gmail.com>
2022-12-12 11:25:29 -0500

    Implementamos un fragment diferente para usuario logueado

--- ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
+++ ajsanta/src/main/java/com/ecommerce/ajsanta/controller/HomeController.java
@@ -56,6 +56,11 @@ public class HomeController {
 
         model.addAttribute("productos", productoService.findAll());
 
+        // session
+        model.addAttribute("sesion", session.getAttribute("idusuario"));
+
+
+
         return "usuario/home";
     }
 
@@ -134,11 +139,16 @@ public class HomeController {
     }
 
     @GetMapping("/getCart")
-    public String getCart(Model model) {
+    public String getCart(Model model, HttpSession session) {
 
 
         model.addAttribute("cart", detalles);
         model.addAttribute("orden", orden);
+
+        // sesion
+        model.addAttribute("sesion", session.getAttribute("idusuario"));
+
+
         return "usuario/carrito";
     }
 
